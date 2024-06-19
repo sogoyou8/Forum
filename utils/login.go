8d3b7359263a247930949caa3c54c6d9797e4343
sogoyou8 <157325709+sogoyou8@users.noreply.github.com>
2024-06-19 23:57:19 +0200

    Add files via upload

---
+++ utils/login.go
@@ -0,0 +1,21 @@
+package Forum
+
+func CheckUserExist(U *EntryUser, E *EntryPost, C *EntryComment, UserID int, username string, password string) {
+	U.IsSignedIn = false
+	U.IsLogged = false
+	E.IsLogged = false
+	C.IsLogged = false
+	for i := 0; i < UserID; i++ {
+		if U.Users[i] == username && U.Passwords[i] == password {
+			U.IsLogged = true
+			E.IsLogged = true
+			C.IsLogged = true
+			U.ID = i
+		} else if U.Emails[i] == username && U.Passwords[i] == password {
+			U.IsLogged = true
+			E.IsLogged = true
+			C.IsLogged = true
+			U.ID = i
+		}
+	}
+}
