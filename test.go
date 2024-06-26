71d5b650cf9ae3a07979590d4e642d38f4de53a3
sogoyou8 <157325709+sogoyou8@users.noreply.github.com>
2024-06-26 23:44:46 +0200

    Commentaires, catégories, et début de likes

--- test.go
+++ test.go
@@ -24,15 +24,12 @@ var ID int
 var CommentID int
 var User string
 var UserID int
-var filtre string
 var username string
 var password string
 var U *forum.EntryUser
 var E *forum.EntryPost
 var C *forum.EntryComment
 var L *forum.Like
-
-var db *sql.DB
 
 func main() {
 
@@ -45,6 +42,7 @@ func main() {
 	http.HandleFunc("/categories", viewCategoryPageHandler)
 	http.HandleFunc("/category", showArticleCategoryPageHandler)
 	http.HandleFunc("/publications", showPublicationPageHandler)
+	http.HandleFunc("/comment", commentPageHandler)
 	http.HandleFunc("/create-category", createCategoryPagePageHandler)
 	http.HandleFunc("/create-post", createPostPageHandler)
 	http.HandleFunc("/profil-self", aprofilPagePageHandler)
@@ -81,7 +79,6 @@ func aProposHandler(w http.ResponseWriter, r *http.Request) {
 
 func loginPageHandler(w http.ResponseWriter, r *http.Request) {
 	Login(w, r)
-	// renderTemplate(w, "2loginPage.html", nil)
 }
 
 func disconnect(w http.ResponseWriter, r *http.Request) {
@@ -99,24 +96,185 @@ func disconnect(w http.ResponseWriter, r *http.Request) {
 }
 
 func viewCategoryPageHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "3viewCategoryPage.html", nil)
+	db, _ := sql.Open("sqlite3", "data/8Forum.db")
+	update = forum.NewDB(db)
+	var categories []string
+
+	rows, err := update.DB.Query("SELECT Name FROM categories;")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var category string
+		rows.Scan(&category)
+		categories = append(categories, category)
+	}
+
+	fmt.Println("categories : ", categories)
+
+	templates, _ := template.New("3viewCategoryPage.html").ParseFiles("templates/3viewCategoryPage.html")
+	templates.ExecuteTemplate(w, "3viewCategoryPage.html", categories)
 }
 
 func showArticleCategoryPageHandler(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "4showArticleCategory.html", nil)
+	E = &forum.EntryPost{}
+	db, _ := sql.Open("sqlite3", "data/8Forum.db")
+	update = forum.NewDB(db)
+
+	update.DB.QueryRow("SELECT MAX(ID) from posts").Scan(&ID)
+	forum.InitialisationPost(E, ID)
+	forum.MajPost(E, update, ID)
+	E.Filter = r.URL.Query().Get("category")
+
+	fmt.Println("Struct : ", E)
+
+	fmt.Println("Filter : ", E.Filter)
+
+	templates, _ := template.New("4showArticleCategory.html").ParseFiles("templates/4showArticleCategory.html")
+
+	templates.ExecuteTemplate(w, "4showArticleCategory.html", E)
 }
 
 func showPublicationPageHandler(w http.ResponseWriter, r *http.Request) {
-	E := &forum.EntryPost{}
+	E = &forum.EntryPost{}
+	db, _ := sql.Open("sqlite3", "data/8Forum.db")
+	update = forum.NewDB(db)
+
+	// Synchronisation struct/BDD (import from BDD)
+	update.DB.QueryRow("SELECT MAX(ID) from posts").Scan(&ID)
+	forum.InitialisationPost(E, ID)
+	forum.MajPost(E, update, ID)
+	fmt.Println("Text : "+E.Text+"\nUser : ", E.User)
+	fmt.Println("Struct : ", E)
+	templates, _ := template.New("5showPublication&CommentPage.html").ParseFiles("templates/5showPublication&CommentPage.html")
+	fmt.Println("E.Filter : ", E.Filter)
+
+	// Affichage basique de la page
+	templates.ExecuteTemplate(w, "5showPublication&CommentPage.html", E)
+
+}
+
+func commentPageHandler(w http.ResponseWriter, r *http.Request) {
+	L = &forum.Like{}
+	db, _ := sql.Open("sqlite3", "data/8Forum.db")
+	update = forum.NewDB(db)
+	U = &forum.EntryUser{}
+
+	update.DB.QueryRow("SELECT MAX(ID) from users").Scan(&UserID)
+	forum.InitialisationUsers(U, UserID)
+	forum.MajUsers(U, update, UserID, U.Email, U.Password, U.Username)
+
+	// Synchronisation struct/BDD (import from BDD)
+	update.DB.QueryRow("SELECT MAX(id) from posts").Scan(&ID)
 
 	forum.InitialisationPost(E, ID)
-	forum.MajPost(E, update, ID, E.User[ID], E.Filter[ID])
-	templates, _ := template.New("5showPublication&CommentPage.html").ParseFiles("templates/5showPublication&CommentPage.html")
-	templates.ExecuteTemplate(w, "5showPublication&CommentPage.html", E)
+	forum.MajPost(E, update, ID)
+
+	update.DB.QueryRow("SELECT MAX(CommentID) from comment").Scan(&CommentID)
+	forum.InitialisationComment(C, CommentID)
+	forum.MajComment(C, update, CommentID, User)
+
+	postId, _ := strconv.Atoi(r.URL.Query().Get("postID"))
+	C = forum.LienCommentPost(C, E, postId)
+
+	userId, err := r.Cookie("userId")
+	if err != nil {
+		C.IsLogged = false
+	} else {
+		C.IsLogged = true
+		update.DB.QueryRow("SELECT Username from users WHERE ID = ?", userId.Value).Scan(&username)
+		fmt.Println("SELECT Username from users WHERE ID = ", userId.Value)
+		fmt.Println("userId : ", userId.Value)
+		fmt.Println("USERNAME : ", username)
+
+	}
+	id, _ := strconv.Atoi(userId.Value)
+	update.IsLiked(E, username, id)
+	C.PostIsLiked = E.IsLiked
+
+	C.PostLikes = forum.NbLikesPost(id, U, update)
+
+	fmt.Println("C : ", C)
+	fmt.Println("E : ", E)
+	fmt.Println("U : ", U)
+	templates, _ := template.New("11commentPage.html").ParseFiles("templates/11commentPage.html")
+
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(w, "11commentPage.html", C)
+	case "POST":
+
+		update.DB.QueryRow("SELECT ID FROM from posts WHERE text = ?", C.PostText).Scan(&ID)
+		forum.GetLikesFromPost(L, update, ID)
+		for i := 0; i < len(L.User); i++ {
+			if L.User[i] == C.PostUser && L.Like[i] == "true" {
+				E.IsLiked = true
+				C.PostIsLiked = true
+			} else {
+				E.IsLiked = false
+				C.PostIsLiked = false
+			}
+		}
+		C.PostIsLiked = E.IsLiked
+		like := r.FormValue("like")
+
+		com := r.FormValue("commentaire")
+		if com != "" {
+			forum.StockageStructComment(C, postId, CommentID, com, username)
+			CommentID += 1
+			update.AddComment(C.ID[0], CommentID, com, username)
+			fmt.Println("C.ID = ", C.ID)
+			fmt.Println("E.ID = ", E.ID)
+			fmt.Println("C.POSTTEXT = ", C.PostText)
+
+		} else {
+			if like == "false" {
+				E.IsLiked = false
+				C.PostIsLiked = E.IsLiked
+				update.AddLike(postId, username)
+				fmt.Println("like added at username " + username)
+				templates.Execute(w, C)
+			} else if like == "true" {
+				E.IsLiked = true
+				C.PostIsLiked = E.IsLiked
+				update.RemoveLike(postId, username)
+				fmt.Println("like removed")
+				templates.Execute(w, C)
+			}
+		}
+		println("like : ", like)
+	}
+
 }
 
 func createCategoryPagePageHandler(w http.ResponseWriter, r *http.Request) {
+	db, _ := sql.Open("sqlite3", "data/8Forum.db")
+	update = forum.NewDB(db)
+
+	var categoryId int
+
+	update.DB.QueryRow("SELECT MAX(ID) from categories").Scan(&categoryId)
+	switch r.Method {
+	case "POST":
+		name := r.FormValue("category-name")
+		description := r.FormValue("category-description")
+		fmt.Println("name : ", name, " description : ", description)
+
+		stmt, err := update.DB.Prepare(`INSERT INTO categories (ID, Name, Description) VALUES (?, ?, ?)`)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		stmt.Exec(strconv.Itoa(categoryId+1), name, description)
+
+		http.Redirect(w, r, "../", http.StatusAccepted)
+	}
+	fmt.Println("Create category")
 	renderTemplate(w, "6createCategoryPage.html", nil)
+
 }
 
 func createPostPageHandler(w http.ResponseWriter, r *http.Request) {
@@ -125,6 +283,20 @@ func createPostPageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "../login", http.StatusSeeOther)
 		return
 	}
+
+	db, _ := sql.Open("sqlite3", "data/8Forum.db")
+	update = forum.NewDB(db)
+
+	var categories []string
+	rows, err := update.DB.Query("SELECT Name from categories")
+	for rows.Next() {
+		var category string
+		rows.Scan(&category)
+		categories = append(categories, category)
+	}
+	U.Categories = categories
+
+	fmt.Println("U.Categories : ", U.Categories)
 	templates, _ := template.New("7createPostPage.html").ParseFiles("templates/7createPostPage.html")
 	switch r.Method {
 	case "GET":
@@ -132,17 +304,23 @@ func createPostPageHandler(w http.ResponseWriter, r *http.Request) {
 		// Affichage basique de la page
 		templates.ExecuteTemplate(w, "7createPostPage.html", U)
 	case "POST":
+		var user string
 		// Récupération des entrées
 		text := r.FormValue("post-content")
-		user := cookie.Value
+		userid := cookie.Value
 		filter := r.FormValue("post-category")
 
+		err := update.DB.QueryRow("SELECT username from users WHERE ID = ?", userid).Scan(&user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		println("content : " + text + "\nauthor : " + user + "\ncategory : " + filter)
 
 		//TODO - implémenter une fonction de hashage pour la sécurité (BONUS)
 
 		// Synchronisation struct/BDD (import entrées to Struct & export entrées to BDD)
-		if username != "" {
+		if user != "" {
 			forum.StockageStructPost(E, ID, text, user, filter)
 			ID += 1
 			update.AddPost(ID, text, user, filter)
@@ -204,8 +382,15 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	update.DB.QueryRow("SELECT MAX(ID) from posts").Scan(&ID)
 	forum.InitialisationPost(E, ID)
-	forum.MajPost(E, update, ID, User, filtre)
+	forum.MajPost(E, update, ID)
 	update.LikeUser(U, E)
+
+	_, err := r.Cookie("userId")
+	if err != nil {
+		U.IsLogged = false
+	} else {
+		U.IsLogged = true
+	}
 
 	templates, _ := template.New("1landingPage.html").ParseFiles("templates/1landingPage.html")
 	// TODO - Gérer le bouton disconnect
@@ -234,7 +419,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 	update.DB.QueryRow("SELECT MAX(ID) from posts").Scan(&ID)
 	forum.InitialisationPost(E, ID)
-	forum.MajPost(E, update, ID, User, filtre)
+	forum.MajPost(E, update, ID)
 
 	forum.CheckUserExist(U, E, C, UserID, username, password)
 
